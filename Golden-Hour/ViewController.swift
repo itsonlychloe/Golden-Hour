de4c6a4b303215a1d57ee3b014e09e875883f607
Chloe Costello (s5125936) <s5125936@mac42943.mac.bournemouth.ac.uk>
2019-05-09 09:14:19 +0100

    Initial Commit

---
+++ Golden-Hour/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Golden-Hour
+//
+//  Created by Chloe Costello (s5125936) on 09/05/2019.
+//  Copyright © 2019 Chloe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
