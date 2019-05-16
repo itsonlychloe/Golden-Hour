4f6e6d5dcc662f96dce6a4e34171382ccc7673d4
Aleksandra Gocal (s5110889) <s5110889@mac42936.mac.bournemouth.ac.uk>
2019-05-16 10:11:36 +0100

    Added information and photos + text constraints

--- Golden-Hour/CustomAnnotation.swift
+++ Golden-Hour/CustomAnnotation.swift
@@ -6,14 +6,17 @@ class CustomAnnotation: NSObject, MKAnnotation {
     
     var coordinate: CLLocationCoordinate2D
     var title: String?
+    var textView: String?
+    
     
     init(document: DocumentSnapshot) {
         let data = document.data()!
         title = data["Name"] as? String
         let geoPoint = data["Coordinate"] as! GeoPoint
         coordinate = CLLocationCoordinate2D(latitude: geoPoint.latitude, longitude: geoPoint.longitude)
-        
-    }
+        textView = data ["link"] as? String
+   
+     }
     
 }
 
--- Golden-Hour/SecondViewController.swift
+++ Golden-Hour/SecondViewController.swift
@@ -5,16 +5,24 @@ class SecondViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var label: UILabel!
-    
+    @IBOutlet weak var textView: UITextView!
     var annotation: CustomAnnotation!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         label.text = annotation.title!
+        textView.text = annotation.textView!
         imageView.image = UIImage(named: annotation.title!)
         imageView.clipsToBounds = true
         
-       
+        
+        imageView.layer.cornerRadius=20
+        imageView.clipsToBounds=true
+        
+        
+        textView.layer.cornerRadius=20
+        textView.clipsToBounds=true
+        
         
     }
     
